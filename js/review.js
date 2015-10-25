4b8bccfddf0fbf0f2d56e0613eb448a2f4e56b33
NICK BUTLER BOHALL <Nick@NICKs-iMac.hsd1.wa.comcast.net>
2015-10-24 20:13:03 -0700

    Made the basic skeleton

---
+++ js/review.js
@@ -0,0 +1,7 @@
+Parse.initialize('tg9Ax8JRS0DdO7e9IHPLyIw2M9fz2Txs5TPjl2q4','p4A5EfNznc5jdR991CmU8mvXkNCMAitLny0CD1eL');
+
+var review = Parse.Object.extend('Review');
+
+$(document).ready(function(){
+	$('#star').raty();
+})
